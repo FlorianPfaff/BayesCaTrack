7cf4fdd43eb651308eb0fc74f6474d945c38ffd7
Florian Pfaff <FlorianPfaff@users.noreply.github.com>
2026-04-26 00:26:17 +0200

    Reuse evaluation metric exports

--- src/bayescatrack/evaluation/__init__.py
+++ src/bayescatrack/evaluation/__init__.py
@@ -1,23 +1,14 @@
 """Evaluation helpers for BayesCaTrack benchmarks."""
 
-from .complete_track_scores import (
-    complete_track_set,
-    normalize_track_matrix,
-    pairwise_track_set,
-    score_complete_tracks,
-    score_pairwise_tracks,
-    score_track_matrices,
-    summarize_tracks,
-    track_lengths,
-)
+from . import complete_track_scores as _scores
 
-__all__ = [
-    "complete_track_set",
-    "normalize_track_matrix",
-    "pairwise_track_set",
-    "score_complete_tracks",
-    "score_pairwise_tracks",
-    "score_track_matrices",
-    "summarize_tracks",
-    "track_lengths",
-]
+complete_track_set = _scores.complete_track_set
+normalize_track_matrix = _scores.normalize_track_matrix
+pairwise_track_set = _scores.pairwise_track_set
+score_complete_tracks = _scores.score_complete_tracks
+score_pairwise_tracks = _scores.score_pairwise_tracks
+score_track_matrices = _scores.score_track_matrices
+summarize_tracks = _scores.summarize_tracks
+track_lengths = _scores.track_lengths
+
+__all__ = _scores.__all__
