ceaad89b582ab8259a1bdbc9d5e8224cb08d293a
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-01 09:55:25 +0200

    Install calibrated Mahalanobis bundle patch

--- src/bayescatrack/association/__init__.py
+++ src/bayescatrack/association/__init__.py
@@ -2,5 +2,6 @@
 
 from .._exports import ASSOCIATION_PUBLIC_NAMES, reexport
 from ..core import bridge as _bridge
+from . import _calibrated_mahalanobis_bundle_patch as _calibrated_mahalanobis_bundle_patch  # noqa: F401
 
 __all__ = reexport(_bridge, globals(), ASSOCIATION_PUBLIC_NAMES)
